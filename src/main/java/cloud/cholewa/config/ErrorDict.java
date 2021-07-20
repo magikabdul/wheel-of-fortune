687a6c7108ed06bb18598e4ebb3b0980071f6d86
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-07-20 20:05:53 +0200

    [WOF-US04] added error message dictionary class

---
+++ src/main/java/cloud/cholewa/config/ErrorDict.java
@@ -0,0 +1,6 @@
+package cloud.cholewa.config;
+
+public class ErrorDict {
+    public static final String PLAYER_CREATE_ERROR_CANT_BE_NULL = "Name can not be null";
+    public static final String PLAYER_CREATE_ERROR_CANT_BE_EMPTY_STRING = "Name can not be empty string";
+}
