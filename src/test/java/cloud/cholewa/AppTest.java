b64087cafb0ab89802b1519c6b45183e3a6c9f89
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-07-20 19:02:37 +0200

    [WOF] first test

---
+++ src/test/java/cloud/cholewa/AppTest.java
@@ -0,0 +1,17 @@
+package cloud.cholewa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void alwaysPass() {
+        //given
+
+        //when
+        //then
+        assertTrue(true);
+    }
+}
