21446f441d1ae5894bcef5cdfd494dca9019e454
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-07-23 20:29:24 +0200

    [WOF-US05] added class for console message show, and separate file with game messages

---
+++ src/main/java/cloud/cholewa/game/messages/GameMessage.java
@@ -0,0 +1,15 @@
+package cloud.cholewa.game.messages;
+
+public class GameMessage {
+
+    public static String WELCOME_MESSAGE = "Witaj w \"Kole Fortuny\"";
+
+    public static String ERROR_ENTERED_VALUE_IS_NOT_NUMBER = "Wprowadzona wartość nie jest liczbą!!!";
+
+    public static String ENTER_NUMBER_OF_PLAYERS = "Podaj liczbę graczy (2-4): ";
+    public static String INVALID_NUMBER_OF_PLAYERS = "Błędna ilość graczy !";
+
+    public static String ENTER_PLAYER_NAME = "Podaj imię gracza nr ";
+
+    public static String GAME_END = "Gra zakończona !";
+}
