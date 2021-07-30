96378919d4ef14870c824cbaf0be5e27b3cfd076
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-07-30 21:11:50 +0200

    [WOF-US07] removed initialization of list

--- src/main/java/cloud/cholewa/game/components/PasswordManager.java
+++ src/main/java/cloud/cholewa/game/components/PasswordManager.java
@@ -1,21 +1,20 @@
 package cloud.cholewa.game.components;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class PasswordManager {
-    private final List<String> passwords = new ArrayList<>();
+    private final List<String> passwords;
 
     public PasswordManager() {
-        passwords.addAll(List.of(
+        passwords = List.of(
                 "Apetyt rośnie w miarę jedzenia",
                 "Co dwie głowy, to nie jedna",
                 "Ćwiczenie czyni mistrza",
                 "Darowanemu koniowi w zęby się nie zagląda",
                 "Diabeł tkwi w szczegółach",
                 "Elektryka prąd nie tyka"
-        ));
+        );
     }
 
     public String getRandomPassword() {
